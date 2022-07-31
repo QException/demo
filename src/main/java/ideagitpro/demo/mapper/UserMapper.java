332039dab29eef66be5d95fee3b17462ab848e47
Jack <emailWithJack@163.com>
2022-07-31 13:43:52 +0800

    增加两个java文件

---
+++ src/main/java/ideagitpro/demo/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package ideagitpro.demo.mapper;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @program: demo
+ * @description:
+ * @author: Mr.qin
+ * @create: 2022-07-31 13:36
+ **/
+public class UserMapper {
+    List<Integer> ad(){return new ArrayList<>();
+    }
+}
---
+++ src/main/java/ideagitpro/demo/mapper/ads.java
@@ -0,0 +1,15 @@
+package ideagitpro.demo.mapper;
+
+import java.util.List;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: Mr.qin
+ * @create: 2022-07-31 13:39
+ **/
+public class ads {
+    public static void main(String[] args) {
+
+    }
+}
